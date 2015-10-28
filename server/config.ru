1338aefa024b5486d06b836ebacb36126fac7202
lostpupil <ufozhengli@163.com>
2015-10-28 16:33:46 +0800

    server

---
+++ server/config.ru
@@ -0,0 +1,4 @@
+require "./server"
+
+run Cuba
+# shotgun --server=thin --port=3000 config.ru
---
+++ server/server.rb
@@ -0,0 +1,28 @@
+require "cuba"
+require "cuba/safe"
+require "cuba/render"
+require 'erb'
+
+require 'yaml'
+# database
+require 'leancloud-ruby-client'
+# dev
+require 'awesome_print'
+Cuba.use Rack::Session::Cookie, :secret => "__a_very_long_string__"
+Cuba.plugin Cuba::Safe
+Cuba.plugin Cuba::Render
+
+config = YAML.load_file('config/leancloud.yml')["leancloud"]
+
+
+Cuba.define do
+  AV.init :application_id => config["appid"],
+    :api_key        => config["appkey"],
+    :quiet           => false
+
+  on get do
+    on root do
+      render 'index'
+    end
+  end
+end
